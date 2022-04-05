4a3b5e7193f338c9e26e2469516a5d854d6c928e
RadoBabo <dominikradojevic@web.de>
2022-04-05 10:36:54 +0200

    test

--- Tutorium/src/Vektor.java
+++ Tutorium/src/Vektor.java
@@ -2,35 +2,33 @@ import java.util.Arrays;
 
 public class Vektor {
 	public static void main(String[] args) {
-	
-		
+
 		int[] vektor1 = {5, 6, 7, 8};
 		int[] vektor2 = {1, 2, 3, 4};
-		
+
 		int [] Vektor3 = vektorprodukt(vektor1, vektor2);
 		System.out.println(Arrays.toString(Vektor3));
 	}
-	
+
 	public static void skalar(int [] vektor, int skalar) {
 		for(int i = 0; i < vektor.length; i++) {
 			vektor[i] = vektor[i] * skalar;
 		}
 	}
-	
+
 	public int [] sumVektors(int [] vektor1, int [] vektor2) {
 		if(lengthCheck(vektor1, vektor2) == false) {
-			System.out.println("Nicht die selbe Länge!");
-			
+			System.out.println("Nicht die selbe Laenge!");
 		}
 
 		int [] returnVektor = new int [vektor1.length];
 		for(int i= 0; i < vektor1.length; i++) {
 			returnVektor[i] = vektor1[i] + vektor2[i];
 		}
-		
+
 		return returnVektor;
 	}
-	
+
 	public static int [] vektorprodukt(int [] vektor1, int [] vektor2) {
 		int [] returnVektor = new int [vektor1.length];
 		returnVektor[0] = (vektor1[1] * vektor2[2]) - (vektor1[2] * vektor2[1]);
@@ -39,7 +37,7 @@ public class Vektor {
 
 		return returnVektor;
 	}
-	
+
 	public static boolean lengthCheck(int [] vektor1, int [] vektor2) {
 		if(vektor1.length == vektor2.length) {
 			return true;
--- Tutorium/src/VektorTest.java
+++ Tutorium/src/VektorTest.java
@@ -7,14 +7,14 @@ class VektorTest {
 
 	int[] vektor1 = {5, 6, 7, 8};
 	int[] vektor2 = {1, 2, 3, 4};
-	
+
 	@Test
 	public void sumVektorTest() {
 		int[] sumVektor = {6, 8, 10, 12};
 		Vektor v = new Vektor();
-		Assert.assertArrayEquals(v.sumVektors(vektor1, vektor2 ), sumVektor);
+		Assert.assertArrayEquals(v.sumVektors(vektor1, vektor2), sumVektor);
 	}
-	
+
 	@Test
 	public void Vektorprodukt() {
 		int[] produktVektor = {4, -8, 4, 0};
@@ -27,7 +27,7 @@ class VektorTest {
 		int[] skalarVektor = {15, 18, 21, 24};
 		Vektor v = new Vektor();
 		v.skalar(vektor1, 3);
-		
+
 		Assert.assertArrayEquals(vektor1, skalarVektor);
 		}
 }
